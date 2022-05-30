053b3fe69fb1555b4a0c33cb6d306521b08f3b15
Claudio <57859613+ClaPalo@users.noreply.github.com>
2022-05-30 16:37:08 +0200

    Aggiunti controlli sulla registrazione. Nella login ora inserire credenziali sbagliate non comporta la rimozione dell'username inserito

--- src/main/java/it/polimi/tiw/controllers/CreateUser.java
+++ src/main/java/it/polimi/tiw/controllers/CreateUser.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +22,7 @@ import it.polimi.tiw.dao.UserDAO;
  * Servlet implementation class CreateUser
  */
 @WebServlet("/CreateUser")
+@MultipartConfig
 public class CreateUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
@@ -62,7 +64,7 @@ public class CreateUser extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String username = request.getParameter("username");
-		String password = request.getParameter("password");
+		String password = request.getParameter("registrationPassword");
 		String mail = request.getParameter("mail");
 		String repPassword = request.getParameter("confirmPassword");
 		
@@ -70,6 +72,7 @@ public class CreateUser extends HttpServlet {
 				username.isEmpty() || password.isEmpty() || mail.isEmpty() || repPassword.isEmpty()) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			response.getWriter().println("Devi inserire tutti i dati richiesti");
+			System.out.println("Mancano dati");
 			return;
 		}
 		
@@ -77,6 +80,7 @@ public class CreateUser extends HttpServlet {
 		if (!isValidMail(mail)) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			response.getWriter().println("Mail non valida");
+			System.out.println("Mail");
 			return;
 		}
 		
@@ -86,23 +90,28 @@ public class CreateUser extends HttpServlet {
 			if (userDao.isPresentUsername(username)) {
 				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 				response.getWriter().println("Username gia presente");
+				System.out.println("Username gia presente");
 				return;
 			} else if (userDao.isPresentMail(mail)) {
 				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 				response.getWriter().println("E-mail gia presente");
+				System.out.println("E-mail gia presente");
 				return;
 			} else if (password.length() < 5) {
 				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 				response.getWriter().println("La password deve essere di almeno 5 caratteri");
+				System.out.println("La password deve essere di almeno 5 caratteri");
 				return;
 			} else if (!password.equals(repPassword)) {
 				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 				response.getWriter().println("Le password devono coincidere");
+				System.out.println("Le password devono coincidere");
 				return;
 			}
 		} catch (SQLException e) {
 			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			response.getWriter().println("Si è verificato un errore nel server, riprova più tardi");
+			System.out.println("Si è verificato un errore nel server, riprova più tardi");
 			return;
 		}
 		
@@ -111,6 +120,7 @@ public class CreateUser extends HttpServlet {
 		} catch (SQLException e) {
 			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			response.getWriter().println("Si è verificato un errore nel server, riprova più tardi");
+			System.out.println("Si è verificato un errore nel server, riprova più tardi");
 			return;
 
 		}
--- src/main/webapp/loginAndRegistration.js
+++ src/main/webapp/loginAndRegistration.js
@@ -3,34 +3,46 @@
 	var loginForm = document.getElementById("loginForm");
 	var registrationForm = document.getElementById("registrationForm");
 	var loginButton = document.getElementById("loginButton");
+	var registrationButton = document.getElementById("registrationButton");
 	var createAccountLabel = document.getElementById("createAccountLabel");
 	var alreadyUserLabel = document.getElementById("alreadyUserLabel");
 	var registrationPassword = document.getElementById("registrationPassword");
 	var confirmPassword = document.getElementById("confirmPassword");
 	var mail = document.getElementById("mail");
-
+	var title = document.getElementById("title");
+	
+	var readyToSend;
+	
+	function start() {
 		//Imposto la view iniziale
 		registrationForm.style.display = "none";
 		loginForm.style.display = "inline";
+		title.textContent = "Login";
+		document.getElementById("utenteCreato").style.display = "none";
+		for (let i = 0; i < document.getElementsByClassName("error").length; i++) {
+			let element = document.getElementsByClassName("error").item(i);
+			element.style.display = "none";
+			element.style.color = "red";
+		}
+	}
+		
+		start();
 
 		createAccountLabel.style.color = "blue";
 		alreadyUserLabel.style.color = "blue";
 
-	for (let i = 0; i < document.getElementsByClassName("error").length; i++) {
-		let element = document.getElementsByClassName("error").item(i);
-		element.style.display = "none";
-		element.style.color = "red";
-	}
+		
 
 		//Aggiungo i listener
 		createAccountLabel.addEventListener("click", ()=>{
 			loginForm.style.display = "none";
 			registrationForm.style.display = "inline";
+			title.textContent = "Registration";
+			
 		})
 
 		alreadyUserLabel.addEventListener("click", ()=>{
-			registrationForm.style.display = "none";
-			loginForm.style.display = "inline";
+			start();
 		})
 
 		createAccountLabel.addEventListener("mouseover", ()=>{
@@ -49,6 +61,7 @@
 
 		loginButton.addEventListener("click", (e)=>{
 			if (loginForm.checkValidity()) {
+				let oldUsername = document.getElementById("nickname").value;
 				makeCall("POST", "CheckLogin", loginForm, function(x) {
 					if (x.readyState === XMLHttpRequest.DONE) {
 						var message = x.responseText;
@@ -59,12 +72,21 @@
 								window.location.href = "Home.html";
 								break;
 							case 400:
+								start();
+								document.getElementById("nickname").value = oldUsername;
+								document.getElementById("loginError").style.display = "inline";
 								document.getElementById("loginError").textContent = message;
 								break;
 							case 401:
+								start();
+								document.getElementById("nickname").value = oldUsername;
+								document.getElementById("loginError").style.display = "inline";
 								document.getElementById("loginError").textContent = message;
 								break;
 							case 500:
+								start();
+								document.getElementById("nickname").value = oldUsername;
+								document.getElementById("loginError").style.display = "inline";
 								document.getElementById("loginError").textContent = message;
 								break;
 						}
@@ -74,6 +96,39 @@
 				loginForm.reportValidity();
 			}
 		})
+		
+		registrationButton.addEventListener("click", (e)=>{
+			if (registrationForm.checkValidity() && isReadyToSend(registrationForm)) {
+				makeCall("POST", "CreateUser", registrationForm, function(x) {
+					if (x.readyState === XMLHttpRequest.DONE) {
+						var message = x.responseText;
+						console.log(x.status);
+						switch(x.status) {
+							case 200:
+								start();
+								document.getElementById("utenteCreato").style.display="inline";
+								break;
+							case 400:
+								document.getElementById("registrationError").style.display = "inline";
+								document.getElementById("registrationError").textContent = message;
+								break;
+							case 401:
+								document.getElementById("registrationError").style.display = "inline";
+								document.getElementById("registrationError").textContent = message;
+								break;
+							case 500:
+								document.getElementById("registrationError").style.display = "inline";
+								document.getElementById("registrationError").textContent = message;
+								break;
+						}
+					}
+				})
+			} else {
+				registrationForm.reportValidity();
+			}
+		})
+		
+		//TODO Aggiungi a checkvalidity un controllo che non permetta di mandare il form se c'è qualche errore
 
 		registrationPassword.addEventListener("focusout", (e)=>{
 			let passwordError = document.getElementById("passwordError");
@@ -87,6 +142,21 @@
 			let passwordError = document.getElementById("passwordError");
 			if (registrationPassword.value.length >= 5) {
 				passwordError.style.display = "none";
+			}
+		})
+		
+		registrationPassword.addEventListener("focusout", (e)=>{
+			let error = document.getElementById("confirmPasswordError");
+			if (confirmPassword.value.length > 0 && !(confirmPassword.value === registrationPassword.value)) {
+				error.textContent = "Le password devono coincidere";
+				error.style.display = "inline";
+			}
+		})
+		
+		registrationPassword.addEventListener("input", (e)=>{
+			let error = document.getElementById("confirmPasswordError");
+			if (confirmPassword.value === registrationPassword.value) {
+				error.style.display = "none";
 			}
 		})
 		
@@ -123,3 +193,25 @@
 	
 
 }) ();
+
+function isReadyToSend(form) {
+	var toCheck = form.getElementsByTagName("input");
+	//Controllo che tutti i campi siano riempiti
+	for (let i = 0; i < toCheck.length; i++) {
+		if (toCheck[i].value.length === 0) {
+			document.getElementById("registrationError").style.display = "inline";
+			document.getElementById("registrationError").textContent = "Tutti i campi sono obbligatori";
+			return false;
+		} else {
+			document.getElementById("registrationError").style.display = "none";
+		}
+	}
+	//Controllo che non ci siano errori di inserimento
+	toCheck = form.getElementsByClassName("error");
+	for (let i = 0; i < toCheck.length; i++) {
+		if (toCheck[i].style.display !== "none"){
+			return false;
+		}
+	}
+	return true;
+}
